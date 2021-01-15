68f75d739bdc531057a4b8a540460d7e82b5debd
ek <kirbomatik@gmail.com>
2021-01-14 21:52:18 -0500

    Adds photo table and album relation

--- moments/albums/models.py
+++ moments/albums/models.py
@@ -1,6 +1,7 @@
+import pathlib
+
 from django.contrib.auth.models import User
 from django.db import models
-from django.urls import reverse
 
 
 class Album(models.Model):
@@ -13,4 +14,26 @@ class Album(models.Model):
         constraints = [
             models.UniqueConstraint(fields=['name', 'owner'],
                                     name='unique_user_album')
+        ]
+
+
+def upload_to(instance, filename):
+    user = instance.album.owner.username
+    album = instance.album.name
+    ext = pathlib.Path(filename).suffix
+
+    return f"{user}/albums/{album}/{instance.title}{ext}"
+
+
+class Photo(models.Model):
+    title = models.CharField(max_length=140)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE, null=True)
+    image = models.ImageField(upload_to=upload_to, null=True, blank=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['title', 'album'],
+                                    name='unique_album_title'),
+            models.UniqueConstraint(fields=['album', 'image'],
+                                    name='unique_album_img')
         ]
